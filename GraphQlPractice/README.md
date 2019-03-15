44980995c4590e6f4fe00adcdb10c376cd06789b
Tapas Kumar <tapas.kumar@lyearn.com>
2019-03-15 12:09:44 +0530

    Node + React from Single Server

--- GraphQlPractice/README.md
+++ GraphQlPractice/README.md
@@ -1,1 +1,16 @@
 Graphql Practice...
+
+GraphQl With Apollo....
+
+React + Node Running from a Single Server
+
+"scripts": {
+    "start": "node app.js",
+    "server": "nodemon app.js",
+
+    "client": "cd ../client && npm start"
+    // "client": "npm start --prefix ../client 
+    // client is folder name in which we have react
+
+    "dev": "concurrently \"npm run server \" npm run client\"",
+},
--- GraphQlPractice/client/src/App.js
+++ GraphQlPractice/client/src/App.js
@@ -4,8 +4,16 @@ import { ApolloProvider } from 'react-apollo';
 import BookList from './components/BookList';
 import AddBook from './components/AddBook';
 
+/*
+  use uri: '/graphql', because, "proxy" is added in package.json
+  as http://localhost:5000
+
+  If "proxy" was not added,
+  then use uri: "http://localhost:5000/graphql"
+*/
+
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql'
+  uri: '/graphql'
 });
 
 // To do run multiple server on single server
